c4c15495be1ae788ebff0a49e3a935f25226e86b
kshitij <kshitij.singh@pyramidcyber.com>
2020-04-26 01:49:48 +0530

    Remove tty intialization from TTY constructor

--- arch/i386/tty.cpp
+++ arch/i386/tty.cpp
@@ -11,20 +11,7 @@
 TTY::TTY()
 {
 
-		static const size_t VGA_WIDTH = 80;
-		static const size_t VGA_HEIGHT = 25;
-		static uint16_t* const  VGA_MEMORY  = (uint16_t*)0xB8000;
 
-		color = vga_entry_color(VGA_COLOR_LIGHT_GRAY, VGA_COLOR_BLACK);
-		buffer = VGA_MEMORY;
-
-		for(size_t y = 0; y < VGA_HEIGHT; y++){
-				for(size_t x = 0 ; x < VGA_WIDTH; x++){
-						const size_t index = y * VGA_WIDTH + x;
-						buffer[index]  = vga_entry(' ',color);
-				}
-
-		}
 }
 
 void TTY::putchar(char c){
